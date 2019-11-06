cec3ca8b8487a4a2a83a4f48f1a940d800ee4ef1
Vinicius de Sa <vinicius.sa@live.com>
2019-11-06 16:18:06 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,22 @@
+# strict variant
+
+This library was adapted from [cbeck88/strict-variant](https://github.com/cbeck88/strict-variant) to compile to Arduino/ESP32 platform.
+
+Documentation
+=============
+
+On [Chris Beck's github pages](https://cbeck88.github.io/strict-variant/index.html).
+
+Compiler Compatibility
+======================
+
+`strict_variant` targets the C++11 standard.
+
+It is known to work with `gcc >= 4.8` and `clang >= 3.5`, and is tested against `MSVC 2015`.
+
+`strict_variant` can be used as-is in projects which require `-fno-exceptions` and `-fno-rtti`.
+
+Licensing and Distribution
+==========================
+
+**strict variant** is available under the boost software license.
---
+++ examples/StrictVariantBasic/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include "strict_variant/variant.hpp"
+
+using namespace strict_variant;
+
+std::string getString(variant<int, std::string> v);
+int getInt(variant<int, std::string> v);
+void printParams(std::vector<variant<int, std::string>> params);
+
+void setup()
+{
+    Serial.begin(115200);
+
+    printf("Starting ...");
+
+    std::vector<variant<int, std::string>> params;
+
+	variant<int, std::string> param1;
+	param1 = "1031001";
+
+	variant<int, std::string> param2;
+	param2 = 60;
+	
+	params.push_back(param1);
+	params.push_back(param2);
+	
+	printParams(params);
+
+    while (1);
+}
+
+void loop()
+{
+}
+
+void printParams(std::vector<variant<int, std::string>> params)
+{
+    printf("Printing parameters...\n");
+
+    variant<int, std::string> param1 = params[0];
+    printf("Param1: %s\n", getString(param1).c_str());
+
+    variant<int, std::string> vlimCurva;
+    vlimCurva = params[1];
+    printf("Param2: %d\n", getInt(vlimCurva));
+}
+
+int getInt(variant<int, std::string> v)
+{
+    return *(get<int>(&v));
+}
+
+std::string getString(variant<int, std::string> v)
+{
+    return *(get<std::string>(&v));
+}
